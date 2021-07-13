ec5714312c7db80c9a05a2986eb3e44fc35565c6
LuckyWind_sck <luckywindsck@gmail.com>
2021-07-14 05:43:52 +0900

    feat: modify structure of hits array

--- types/search-response.ts
+++ types/search-response.ts
@@ -1,7 +1,14 @@
+interface Hit<T> {
+  id: string;
+  score: number;
+  data: T;
+  highlight: any;
+}
+
 export interface SearchResponse<T> {
   stat: {
     total: number;
     latency: number; // Unit: milliseconds
   };
-  hits: T[]; // | null;
+  hits: Hit<T>[];
 }
