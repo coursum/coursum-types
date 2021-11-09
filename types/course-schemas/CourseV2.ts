f1462591fa35c0ba898d888ab9ac34e4daf237cd
LuckyWind_sck <luckywindsck@gmail.com>
2021-11-09 19:50:59 +0900

    feat: add v2 of course schema

---
+++ types/course-schemas/CourseV2.ts
@@ -0,0 +1,123 @@
+type Nullish = null | undefined;
+
+interface I18nData<DataType = string | Nullish> {
+  ja: DataType;
+  en: DataType;
+}
+
+interface Lecturer {
+  // Column: Lecturer Name
+  name: I18nData;
+}
+
+interface Schedule {
+  // Column: Year/Semester
+  year: number;
+  // Column: Year/Semester
+  semester: I18nData;
+  // Column: Day of Week・Period
+  times: I18nData<(string | Nullish)[]>;
+}
+
+interface EnglishSupport {
+  languageUsed: {
+    lecture: I18nData;
+    material: I18nData;
+    discussion: I18nData;
+    groupWork: I18nData;
+    presentation: I18nData;
+  };
+  japaneseLanguageSkillLevel: {
+    reading: I18nData;
+    writing: I18nData;
+    requiredSkillsForReadingAndWriting: I18nData;
+    speaking: I18nData;
+    listening: I18nData;
+  };
+}
+
+interface Registration {
+  prerequisite: {
+    // Column: Prerequisites -> Prerequisite Courses
+    // Courses that must be taken beforehand
+    mandatory: string | Nullish;
+    // Column: Prerequisites -> Prerequisite Courses(Recommended)
+    // Courses that are recommend to be taken beforehand
+    recommended: string | Nullish;
+  };
+  // Column: Prerequisites
+  requirement: I18nData;
+  // Column: Prerequisite Knowledge
+  suggestion: I18nData;
+  advice: I18nData;
+  // Column: Duplicate Course
+  // No id, only course title
+  duplicateCourses: I18nData;
+}
+
+interface Screening {
+  needScreening: boolean;
+  screening: I18nData;
+  // Column: Expected Number of Students
+  quota: number | Nullish;
+  // Column: Screening Assignment
+  assignment: I18nData;
+}
+
+interface Tag {
+  // Column: Aspect name
+  aspect: I18nData;
+  // Column: Field (Undergraduate)
+  category: I18nData;
+  // Column: Class Format
+  // Online / On campus
+  classFormat: I18nData;
+  // Column: Class Style
+  // Enum: Lecture / Group Work / Workshop / Connecting to Other Sites
+  types: I18nData<(string | Nullish)[]>;
+  // Column: Subject Sort
+  curriculumCode: string | Nullish; // ???
+  // Column: GIGA Certificate
+  giga: boolean | Nullish;
+}
+
+// Available time: 2020 Autumn ~ Now (2021 Autumn)
+// Data source: SYLLABUS SEARCH
+// URL: syllabus.sfc.keio.ac.jp
+export interface CourseV2 {
+  id: string; // non nullish
+  // For seminar, it will be Column: Study Group Theme
+  title: I18nData;
+  // Column: Department Name
+  department: I18nData;
+  lecturers: Lecturer[];
+  schedule: Schedule;
+  // Column: location
+  // Campus
+  location: string | Nullish;
+  // Column: Unit
+  credit: number | Nullish;
+  language: I18nData;
+  englishSupport: EnglishSupport;
+  url: I18nData;
+  // Column: Equipments & Software
+  tools: I18nData;
+  // Column: Materials & Reading List
+  teachingMaterials: I18nData;
+  // Detail -> Course Summary
+  summary: I18nData;
+  // Detail -> Objectives / Intended Learning Outcome
+  objectives: I18nData;
+  // Detail -> Assignments, Examination & Grade Evaluation
+  grading: I18nData;
+  // Class Schedule
+  plan: I18nData;
+  registration: Registration;
+  screening: Screening;
+  // Column: Prerequisites -> Related Courses
+  related: string | Nullish;
+  // Original URL of the syllabus page
+  syllabusURL: string | Nullish;
+  tag: Tag;
+  version: 2;
+}
--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,9 @@
 import { CourseV1 } from './course-schemas/CourseV1';
+import { CourseV2 } from './course-schemas/CourseV2';
 import { SearchResponse } from './search-response';
 
 export {
   CourseV1,
+  CourseV2,
   SearchResponse,
 };
